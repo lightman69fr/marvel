dee35648b6d66853eac395b13999b7b4e6338af2
Guillaume Lightman <guillaume.lightman@gmail.com>
2018-03-08 19:39:59 +0100

    maj

--- gitPush.sh
+++ gitPush.sh
@@ -1,10 +1,13 @@
-!/bin/bash
+#!/bin/bash
 echo "Script de push pour GitHub"
 echo "Veuillez choisir le dossier a envoyer : "
 read dir
-
 cd $dir
 
+echo "Saisissez les informations de mise a jours :"
+read maj
+
+git commit -m $maj
 git add .
 git status
 git push
